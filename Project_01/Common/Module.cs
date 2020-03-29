96c4b99222701e841298b0cfa0610af14b3330f0
myhuangjun <851248673@qq.com>
2020-03-29 09:35:21 +0800

    第一次提交代码

---
+++ Project_01/Common/Module.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    /// <summary>
+    /// 抽象基类
+    /// </summary>
+     public abstract class ABSBaseClass
+    {
+        public abstract void Do();
+    }
+
+    /// <summary>
+    /// 子类
+    /// </summary>
+    public class ABSChild : ABSBaseClass
+    {
+        public override void Do()
+        {
+            
+        }
+    }
+
+    /// <summary>
+    /// 接口
+    /// </summary>
+    public interface IBaseInterface
+    {
+        void Do();
+    }
+    /// <summary>
+    /// 接口的实现
+    /// </summary>
+    public class IChild:IBaseInterface
+    {
+        public void Do()
+        {
+
+        }
+    }
+
+
+}
